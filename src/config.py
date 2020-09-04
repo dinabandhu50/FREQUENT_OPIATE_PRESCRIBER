884d1e9b4a184e6e12937d0782d64f07f9570f9c
dinabandhu50 <beheradinabandhu50@gmail.com>
2020-09-04 09:57:43 +0530

    removed data directory from git track and added config

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os 
+
+# data path management
+PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TRAIN_DATA_PATH = PROJECT_ROOT_DIR + "/data/train.csv"
+TEST_DATA_PATH = PROJECT_ROOT_DIR + "/data/test.csv"
+
+# 
