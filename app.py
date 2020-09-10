1647fc29885bfdd182391e99663dc931f10b32fc
dinabandhu50 <beheradinabandhu50@gmail.com>
2020-09-10 16:22:32 +0530

    api build

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask,render_template, request
-
+from flask import Flask, render_template, request, jsonify
+import joblib
 app = Flask(__name__)
 
-@app.route('/',methods=['POST','GET'])
 
-def home():
+@app.route('/', methods=['GET', 'POST'])
+def front_end():
     return render_template('home.html')
 
+
+@app.route('/pred', methods=['POST'])
+def prediction():
+    data = request.get_json()
+    pred = model_saved.predict(data)
+    return jsonify(str(pred))
+
+
 if __name__ == '__main__':
+    MODEL_PATH = './models/model.save'
+    # with open(MODEL_PATH, 'rb') as fid:
+        # model_saved = pickle.load(fid)
+    model_saved = joblib.load(MODEL_PATH)
     app.run(debug=True)
+    
